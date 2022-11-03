898df15c3d7accc1f275dc918cd84a5fbdf05295
RNDr. Simon Toth <happy.cerberus@gmail.com>
2022-11-03 19:20:06 +0000

    format

--- testing/data_test.cc
+++ testing/data_test.cc
@@ -5,16 +5,19 @@
 int main() {
     std::string error;
     using bazel::tools::cpp::runfiles::Runfiles;
-    std::unique_ptr<Runfiles> runfiles(Runfiles::CreateForTest(&error));
+    std::unique_ptr<Runfiles> runfiles(
+        Runfiles::CreateForTest(&error));
     if (runfiles == nullptr) {
         std::cerr << error << "\n";
         return 1;
     }
 
-    std::string file_path = runfiles->Rlocation("bazel_demo/testing/file.txt");
+    std::string file_path = runfiles->Rlocation(
+        "bazel_demo/testing/file.txt");
     std::fstream data(file_path);
     if (!data.is_open()) {
-        std::cerr << "Failed to open file {" << file_path << "}\n";
+        std::cerr << "Failed to open file {" 
+            << file_path << "}\n";
         return 1;
     }
     
